0c4a87c6d279ee94e6ac5c312dd95fd6bf8117ca
aghani@inahga.org <Ameer Ghani>
2020-11-23 22:50:03 -0600

    Add inahga s3 bucket

--- scaffolding/aws/main.tf
+++ scaffolding/aws/main.tf
@@ -7,3 +7,19 @@ module "route53" {
   source           = "./route53"
   local_ip_address = var.local_ip_address
 }
+
+resource "aws_s3_bucket" "inahga" {
+  bucket = "inahga"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
