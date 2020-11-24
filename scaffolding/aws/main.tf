813e201a02ef9e589e4f6cdb249f3527e46e951a
aghani@inahga.org <Ameer Ghani>
2020-11-23 23:06:41 -0600

    Set security settings on s3 bucket

--- scaffolding/aws/main.tf
+++ scaffolding/aws/main.tf
@@ -23,3 +23,11 @@ resource "aws_s3_bucket" "inahga" {
     }
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "inahga" {
+  bucket                  = aws_s3_bucket.inahga.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
