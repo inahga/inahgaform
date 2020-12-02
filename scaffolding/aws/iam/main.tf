5520d031f5711ac9ce6095646d70696bf7762453
aghani@inahga.org <Ameer Ghani>
2020-12-01 21:20:05 -0600

    feat: add certman configuration

--- scaffolding/aws/iam/main.tf
+++ scaffolding/aws/iam/main.tf
@@ -3,3 +3,43 @@ resource "aws_key_pair" "key_pairs" {
   key_name   = each.key
   public_key = each.value
 }
+
+resource "aws_iam_policy" "certman" {
+  name = "certman"
+  path = "/"
+
+  policy = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "route53:GetChange",
+      "Resource": "arn:aws:route53:::change/*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "route53:ChangeResourceRecordSets",
+        "route53:ListResourceRecordSets"
+      ],
+      "Resource": "arn:aws:route53:::hostedzone/*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": "route53:ListHostedZonesByName",
+      "Resource": "*"
+    }
+  ]
+}
+EOT
+}
+
+resource "aws_iam_user" "certman" {
+  name = "certman"
+}
+
+resource "aws_iam_user_policy_attachment" "certman_attachment" {
+  user       = aws_iam_user.certman.name
+  policy_arn = aws_iam_policy.certman.arn
+}
