83a52fb950be9cdffcb187e5eb8d34b11914b115
Ameer Ghani <inahga@gmail.com>
2020-12-06 00:53:08 -0600

    sunsetting vsphere

--- scaffolding/main.tf
+++ scaffolding/main.tf
@@ -8,23 +8,23 @@ terraform {
   }
 }
 
-provider "vsphere" {
-  user                 = var.vsphere_user
-  password             = var.vsphere_password
-  vsphere_server       = var.vsphere_server
-  allow_unverified_ssl = true
-}
+# provider "vsphere" {
+#   user                 = var.vsphere_user
+#   password             = var.vsphere_password
+#   vsphere_server       = var.vsphere_server
+#   allow_unverified_ssl = true
+# }
 
 provider "aws" {
   region = var.aws_region
   # Use awscli to set the AWS credentials for your session.
 }
 
-module "vsphere" {
-  source        = "./vsphere"
-  ssh_keys      = var.ssh_keys
-  inventory_dir = "${path.module}/inventory/vsphere"
-}
+# module "vsphere" {
+#   source        = "./vsphere"
+#   ssh_keys      = var.ssh_keys
+#   inventory_dir = "${path.module}/inventory/vsphere"
+# }
 
 module "aws" {
   source           = "./aws"
