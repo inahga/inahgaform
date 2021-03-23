b2f0fa03ca8814b734433af112b373cb13d4dc84
Ameer Ghani <inahga@gmail.com>
2021-03-22 23:20:20 -0500

    feat: resources for github backups

--- scaffolding/aws/main.tf
+++ scaffolding/aws/main.tf
@@ -15,6 +15,7 @@ resource "aws_s3_bucket" "inahga" {
   versioning {
     enabled = true
   }
+
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
@@ -22,6 +23,63 @@ resource "aws_s3_bucket" "inahga" {
       }
     }
   }
+
+  lifecycle_rule {
+    id      = "delete-old-github-backups"
+    enabled = true
+    prefix  = "github/"
+
+    expiration {
+      days = 30
+    }
+
+    noncurrent_version_expiration {
+      days = 30
+    }
+  }
+}
+
+resource "aws_s3_bucket_object" "github" {
+  bucket = aws_s3_bucket.inahga.id
+  key    = "github/"
+  source = "/dev/null"
+}
+
+resource "aws_iam_user" "github_put" {
+  name = "github-put"
+}
+
+resource "aws_iam_policy" "github_put" {
+  name = "github-put"
+  path = "/"
+
+  policy = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:PutObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.inahga.arn}/github/*"
+    }
+  ]
+}
+EOT
+}
+
+resource "aws_iam_user_policy_attachment" "github_put" {
+  user       = aws_iam_user.github_put.name
+  policy_arn = aws_iam_policy.github_put.arn
+}
+
+resource "aws_s3_bucket_public_access_block" "inahga" {
+  bucket                  = aws_s3_bucket.inahga.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket" "inahga-public" {
@@ -48,10 +106,3 @@ resource "aws_s3_bucket" "inahga-public" {
 EOF
 }
 
-resource "aws_s3_bucket_public_access_block" "inahga" {
-  bucket                  = aws_s3_bucket.inahga.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
